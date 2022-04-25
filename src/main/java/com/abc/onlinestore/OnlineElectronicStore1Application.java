ae4f4d9e5c2503012038bdff039368a77d088270
kruthi.c <kruthi9963@gmail.com>
2022-04-25 22:47:20 +0530

    my first commit

---
+++ src/main/java/com/abc/onlinestore/OnlineElectronicStore1Application.java
@@ -0,0 +1,13 @@
+package com.abc.onlinestore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineElectronicStore1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineElectronicStore1Application.class, args);
+	}
+
+}
